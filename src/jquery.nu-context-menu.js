e3f5c1fe610150db565d7514044dd4c85c6c3cb4
Alex Suyun <alexsuyunov@yahoo.com>
2015-11-13 14:43:05 -0500

    Reformatted, removed some comments...

--- src/jquery.nu-context-menu.js
+++ src/jquery.nu-context-menu.js
@@ -6,176 +6,186 @@
 ;
 (function($, window, document, undefined) {
 
-    'use strict';
+  'use strict';
 
-    var plugin = 'nuContextMenu';
+  var plugin = 'nuContextMenu';
 
-    var defaults = {
-        contextMenuClass: 'nu-context-menu',
-        activeClass: 'active',
-    };
+  var defaults = {
+    contextMenuClass: 'nu-context-menu',
+    activeClass: 'active',
+  };
 
-    var nuContextMenu = function(container, options) {
-        this.container = $(container);
-        this.options = $.extend({}, defaults, options);
-        this._defaults = defaults;
-        this._name = plugin;
-        this.init();
-    };
+  var nuContextMenu = function(container, options) {
+    this.container = $(container);
+    this.options = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = plugin;
+    this.init();
+  };
 
-    $.extend(nuContextMenu.prototype, {
-        init: function() {
+  $.extend(nuContextMenu.prototype, {
+    init: function() {
 
-            if (this.options.items) {
-                this.items = $(this.options.items);
-            }
+      if (this.options.items) {
+        this.items = $(this.options.items);
+      }
 
-            if (this._buildContextMenu()) {
-                this._bindEvents();
-                this._menuVisible = this._menu.hasClass(this.options.activeClass);
-            }
-        },
+      if (this._buildContextMenu()) {
+        this._bindEvents();
+        this._menuVisible = this._menu.hasClass(this.options.activeClass);
+      }
+    },
 
-        _getCallback: function() {
-            return ((this.options.callback && typeof this.options.callback === 'function') ? this.options.callback : function () {});
-        },
+    _getCallback: function() {
+      return ((this.options.callback && typeof this.options.callback ===
+        'function') ? this.options.callback : function() {});
+    },
 
-        _buildContextMenu: function() {
+    _buildContextMenu: function() {
 
-            if (!this.options.menu || typeof this.options.menu !== 'object') {
-                return false;
-            }
+      if (!this.options.menu || typeof this.options.menu !==
+        'object') {
+        return false;
+      }
 
-            // Create context menu
-            this._menu = $('<div>')
-                .addClass(this.options.contextMenuClass)
-                .append('<ul>');
+      // Create context menu
+      this._menu = $('<div>')
+        .addClass(this.options.contextMenuClass)
+        .append('<ul>');
 
-            var menuObject = this.options.menu,
-                menuList = this._menu.children('ul');
+      var menuObject = this.options.menu,
+        menuList = this._menu.children('ul');
 
-            // Create menu items 
-            $.each(menuObject, function(key, value) {
+      // Create menu items 
+      $.each(menuObject, function(key, value) {
 
-                var item;
+        var item;
 
-                if (value === 'separator') {
-                    item = $('<hr>');
-                }
+        if (value === 'separator') {
+          item = $('<hr>');
+        }
 
-                if (value && typeof value === 'object') {
+        if (value && typeof value === 'object') {
 
-                    item = $('<li>')
-                        .attr('data-key', key)
-                        .text(' ' + value.title);
+          item = $('<li>')
+            .attr('data-key', key)
+            .text(' ' + value.title);
 
-                    // Font-awesome support
-                    if (value.icon) {
-                        var icon = $('<i>')
-                            .addClass('fa fa-' + value.icon.toString());
-                        item.prepend(icon);
-                    }
+          // Font-awesome support
+          if (value.icon) {
+            var icon = $('<i>')
+              .addClass('fa fa-' + value.icon.toString());
+            item.prepend(icon);
+          }
 
-                }
+        }
 
-                menuList.append(item);
+        menuList.append(item);
 
-            });
+      });
 
-            $('body').append(this._menu);
+      $('body')
+        .append(this._menu);
 
-            return true;
+      return true;
 
-        },
+    },
 
-        _pDefault: function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            return false;
-        },
+    _pDefault: function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+    },
 
-        _contextMenu: function(event) {
+    _contextMenu: function(event) {
 
-            event.preventDefault();
+      event.preventDefault();
 
-            var element = event.target;
+      var element = event.target;
 
-            if (this._menuVisible) {
-                return false;
-            }
+      if (this._menuVisible) {
+        return false;
+      }
 
-            if (this.options.disable) {
-                return false;
-            }
+      if (this.options.disable) {
+        return false;
+      }
 
-            // Callback function that will be attached
-            // to the list items 
-            var callback = this._getCallback();
+      var callback = this._getCallback();
+      var listItems = this._menu.children('ul')
+        .children('li');
 
-            var listItems = this._menu.children('ul').children('li');
-            // Prevent memory leak 
-            listItems.off();
+      listItems.off()
+        .on('click', function() {
 
-            listItems.on('click', function() {
-
-                var key = $(this).attr('data-key');
-                callback(key, element);
-            });
-
-            this._menu.addClass(this.options.activeClass);
-            this._menuVisible = true;
-            this._menu.css({
-                'top': event.pageY + 'px',
-                'left': event.pageX + 'px'
-            });
-
-            return true;
-        },
-
-        _onMouseDown: function(event) {
-            // Remove menu if clicked outside
-            if (!$(event.target).parents('.' + this.options.contextMenuClass).length) {
-                this._menu.removeClass(this.options.activeClass);
-                this._menuVisible = false;
-            }
-        },
-
-        _bindEvents: function() {
-
-            if (this.items) {
-                this.items.on('contextmenu', $.proxy(this._contextMenu, this));
-                this.container.on('contextmenu', $.proxy(this._pDefault, this));
-            } else {
-                this.container.on('contextmenu', $.proxy(this._contextMenu, this));
-            }
-
-            // Remove menu on click 
-            $(document).on('mousedown', $.proxy(this._onMouseDown, this));
-
-        },
-
-        disable: function() {
-            this.options.disable = true;
-            return true;
-        },
-
-    });
-
-    $.fn[plugin] = function(options) {
-        var args = Array.prototype.slice.call(arguments, 1);
-
-        return this.each(function() {
-            var item = $(this),
-                instance = item.data(plugin);
-            if (!instance) {
-                item.data(plugin, new nuContextMenu(this, options));
-            } else {
-
-                if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
-                    instance[options].apply(instance, args);
-                }
-            }
+          var key = $(this)
+            .attr('data-key');
+          callback(key, element);
         });
-    };
+
+      this._menu.addClass(this.options.activeClass);
+      this._menuVisible = true;
+      this._menu.css({
+        'top': event.pageY + 'px',
+        'left': event.pageX + 'px'
+      });
+
+      return true;
+    },
+
+    _onMouseDown: function(event) {
+      // Remove menu if clicked outside
+      if (!$(event.target)
+        .parents('.' + this.options.contextMenuClass)
+        .length) {
+        this._menu.removeClass(this.options.activeClass);
+        this._menuVisible = false;
+      }
+    },
+
+    _bindEvents: function() {
+
+      if (this.items) {
+        this.items.on('contextmenu', $.proxy(this._contextMenu,
+          this));
+        this.container.on('contextmenu', $.proxy(this._pDefault,
+          this));
+      }
+      else {
+        this.container.on('contextmenu', $.proxy(this._contextMenu,
+          this));
+      }
+
+      // Remove menu on click 
+      $(document)
+        .on('mousedown', $.proxy(this._onMouseDown, this));
+
+    },
+
+    disable: function() {
+      this.options.disable = true;
+      return true;
+    },
+
+  });
+
+  $.fn[plugin] = function(options) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    return this.each(function() {
+      var item = $(this),
+        instance = item.data(plugin);
+      if (!instance) {
+        item.data(plugin, new nuContextMenu(this, options));
+      }
+      else {
+
+        if (typeof options === 'string' && options[0] !== '_' &&
+          options !== 'init') {
+          instance[options].apply(instance, args);
+        }
+      }
+    });
+  };
 
 })(jQuery, window, document);
